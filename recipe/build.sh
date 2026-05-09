6e5de0fa2ecb2c43aec944cbedc4f50bc503788f
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2026-05-09 11:11:55 +0200

    Add tango-starter-dbg on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,19 @@
-mkdir build
-cd build
 cmake ${CMAKE_ARGS} \
-      -DCMAKE_BUILD_TYPE=Release \
+      -G Ninja \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_VERBOSE_MAKEFILE=true \
-      ..
+      -S . \
+      -B build
 
-make -j $CPU_COUNT
-make install
+cmake --build build
+cmake --install build
+
+# Separate debugging symbols on Linux
+if [ -n "${OBJCOPY}" ]
+then
+  mkdir -p ${PREFIX}/lib/debug
+  ${OBJCOPY} --only-keep-debug ${PREFIX}/bin/Starter ${PREFIX}/lib/debug/Starter.dbg
+  chmod 664 ${PREFIX}/lib/debug/Starter.dbg
+  ${OBJCOPY} --strip-debug ${PREFIX}/bin/Starter
+  ${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/debug/Starter.dbg ${PREFIX}/bin/Starter
+fi
